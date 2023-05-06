626f78913b1577124646b7f20774cbdf3021f02b
Noertri <trinuruns@gmail.com>
2023-05-06 12:44:44 +0700

    mengganti header dg bahasa inggris

--- scraper_rmp_wanted_persons.py
+++ scraper_rmp_wanted_persons.py
@@ -21,23 +21,23 @@ def main():
             table_rows = li.select_one("table").select("tr")
             
             result = {
-                    "nama":         li.select_one("h3").get_text(strip=True, separator=" "),
-                    "nama_gelaran": table_rows[1].select("td")[1].get_text(strip=True, separator=" "),
+                    "name":         li.select_one("h3").get_text(strip=True, separator=" "),
+                    "nickname": table_rows[1].select("td")[1].get_text(strip=True, separator=" "),
                     "no_kp":        table_rows[2].select("td")[1].get_text(strip=True, separator=" "),
-                    "jantina":      table_rows[2].select("td")[3].get_text(strip=True, separator=" "),
-                    "bangsa":       table_rows[3].select("td")[1].get_text(strip=True, separator=" "),
-                    "tarikh_lahir": table_rows[3].select("td")[3].get_text(strip=True, separator=" "),
-                    "alamat":       table_rows[4].select("td")[1].get_text(strip=True, separator=" "),
-                    "repot_no":     table_rows[5].select("td")[1].get_text(strip=True, separator=" "),
-                    "kesalahan":    table_rows[6].select("td")[1].get_text(strip=True, separator=" "),
-                    "catatan":      table_rows[7].select("td")[1].get_text(strip=True, separator=" ").replace("\n", " "),
-                    "photo":        urljoin(base_url, img_tag.get("src"))
+                    "gender":      table_rows[2].select("td")[3].get_text(strip=True, separator=" "),
+                    "nation":       table_rows[3].select("td")[1].get_text(strip=True, separator=" "),
+                    "date_of_birth": table_rows[3].select("td")[3].get_text(strip=True, separator=" "),
+                    "address":       table_rows[4].select("td")[1].get_text(strip=True, separator=" "),
+                    "trouble_no":     table_rows[5].select("td")[1].get_text(strip=True, separator=" "),
+                    "fault":    table_rows[6].select("td")[1].get_text(strip=True, separator=" "),
+                    "notes":      table_rows[7].select("td")[1].get_text(strip=True, separator=" ").replace("\n", " "),
+                    "link_photo":        urljoin(base_url, img_tag.get("src"))
             }
             
             results.append(result)
     
     file_name = "Royal_Malaysia_Police_Wanted_Persons_{0}.csv".format(datetime.now().strftime("%d%m%Y%H%M%S"))
-    field_names = ("nama", "nama_gelaran", "no_kp", "jantina", "bangsa", "tarikh_lahir", "alamat", "repot_no", "kesalahan", "catatan", "photo")
+    field_names = ("name", "nickname", "no_kp", "gender", "nation", "date_of_birth", "address", "trouble_no", "fault", "notes", "link_photo")
     print(f"Save to {file_name}...")
     try:
         with open(file_name, "w", newline="", encoding="utf-8") as f:
